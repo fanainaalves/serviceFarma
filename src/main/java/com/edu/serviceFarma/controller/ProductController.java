a3423c926a08a2a0cb14964fd6bfb14712aff80e
Fanaina Alves <fanaiina.allves@gmail.com>
2024-08-12 09:38:30 -0300

    Criando a classe de CORS

--- src/main/java/com/edu/serviceFarma/controller/ProductController.java
+++ src/main/java/com/edu/serviceFarma/controller/ProductController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/product")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     @Autowired
